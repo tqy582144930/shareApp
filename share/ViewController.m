dde6c6515bbd23a2b2635db7742833db773d8452
tqy582144930 <39591995+tqy582144930@users.noreply.github.com>
2018-08-03 16:15:41 +0800

    Add files via upload

---
+++ share/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  share
+//
+//  Created by tuqiangyao on 2018/7/27.
+//  Copyright © 2018年 tuqiangyao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
